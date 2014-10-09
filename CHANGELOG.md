06b9af945c4f8232dc3ddd1a8fac8da509960702
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-10-09 08:23:06 -0700

    Released 0.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 0.2
+ 
+The release is the same as ```0.1.1``` but ```0.1.1``` is wrong release acording
+to the semantic versioning specification
 
 ###0.1.1
 
--- __init__.php
+++ __init__.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/hyyan/admin-color-schema/
  * Description: A wordpress plugin to enable wordpress themes to add its own admin color schema directly from theme
  * Author: Hyyan Abo Fakher
- * Version: 0.1.1
+ * Version: 0.2
  * Author URI: https://github.com/hyyan
  * GitHub Plugin URI: hyyan/admin-color-schema
  * License: MIT License
